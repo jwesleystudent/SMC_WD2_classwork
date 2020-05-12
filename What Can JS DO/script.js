647db91414c96cae76ec7ce60871ec8ca5d3db1e
jwesleystudent <61220830+jwesleystudent@users.noreply.github.com>
2020-05-11 17:23:25 -0700

    worked n in class stuff
    made buttons work and switched pictures

--- What Can JS DO/script.js
+++ What Can JS DO/script.js
@@ -2,6 +2,9 @@ var header= document.getElementById("docHeader");
 
 var hColorChangeBtn = document.getElementById("headerColorControl");
 
+var frenchieImg = document.getElementById("frenchieImg");
+var puppyButton= document.getElementById("puppyImgChange");
+
 var headerColorChange = function(){
     var redComp = Math.random() * 255;
     console.log(redComp)
@@ -14,6 +17,27 @@ var headerColorChange = function(){
     header.style.backgroundColor="rgb(" + redComp  + ", "+ greenComp +", " + blueComp + ")";
 }
 
+var swapPuppyImage = function(){
+    console.log(frenchieImg.alt);
+    if(frenchieImg.alt == "Image of a cute Frenchie puppy"){
+        frenchieImg.src = "images/corgi-puppy.jpg";
+        frenchieImg.alt= "Image of a cute Corgi puppy";
+        frenchieButton.innerText="Show the First One"
+    }
 
+    else {
+        frenchieImg.src = "images/french-puppy.jpg";
+        frenchieImg.alt= "Image of a cute Frenchie puppy";
+        frenchieButton.innerText="Show the Second One"
+    }
 
+    frenchieImg.src = "images/corgi-puppy.jpg";
+    puppyButton.innerText="Show the first one";
+}
+
+// just when JS first loads
+headerColorChange();
+// on button click
 hColorChangeBtn.addEventListener("click", headerColorChange);
+
+puppyButton.addEventListener("click", swapPuppyImage);

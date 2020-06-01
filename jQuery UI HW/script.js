477a2a56a3be9801712221d1e77f378dc0dd2b25
jwesleystudent <61220830+jwesleystudent@users.noreply.github.com>
2020-06-01 16:15:31 -0700

    Playing with Anime
    added plaiyng with anime and added images

--- jQuery UI HW/script.js
+++ jQuery UI HW/script.js
@@ -5,5 +5,43 @@ $( function() {
     })
   } );
  
+
+
+
+  // playing w anime tab
+
+  $("#randomBtn").click(function(){
+    var images = [];
+    var imgElmt = document.getElementsByTagName("img");
+
+    for(var i = 0; i < imgElmt.length; i++ ){
+        images[i] = imgElmt[i].src;
+    }
+
+    for(var i = 0; i < 4; i++ ){
+        var pos = Math.floor(Math.random()*images.length);
+
+        imgElmt[i].src = images[pos];
+        images.splice(pos, 1);
+        console.log(images);
+    }
+    
+})
+
+$("#fadeBtn").click(function(){
+    $("#pic4").fadeOut(1000);    
+})
+
+$("#shrinkBtn").click(function(){
+$("#pic3").slideUp(1000);
+})
+
+$("#flipBtn").click(function(){
+    $("#pic2").animate({rotate:"360deg"},1000)
+})
+$("#resetBtn").click(function() {
+location.reload();
+});
   
 })
+
